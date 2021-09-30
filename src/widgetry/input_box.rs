721deb057f1c22e3c604e9ebcbbde617c1f2a5f8
Nyeogmi <economicsbat@gmail.com>
2021-09-30 11:23:10 -0700

    Highlighting for input boxes

--- src/widgetry/input_box.rs
+++ src/widgetry/input_box.rs
@@ -7,7 +7,8 @@ pub type InputBox = Widget<InputBoxState>;
 
 pub struct InputBoxState {
     text: String,
-    cursor: usize,
+    cursor_l: usize,
+    cursor_r: usize,
 
     // TODO: Left position of window
     // TODO: Highlighting (click/drag etc)
@@ -17,7 +18,8 @@ impl Default for InputBoxState {
     fn default() -> Self {
         Self { 
             text: "".to_owned(),
-            cursor: 0,
+            cursor_l: 0,
+            cursor_r: 0,
         }
     }
 }
@@ -39,6 +41,17 @@ impl Widgetlike for InputBoxState {
                 },
                 MouseEvent::Click(_, _, _) => {}
                 MouseEvent::Up(_, _, _) => {}
+                MouseEvent::Drag {
+                    mouse_button: MouseButton::Left,
+                    start_point, now_point, 
+                    ..
+                } => {
+                    if start_point.x < 0 { return; } // should be impossible
+                    let now_x = now_point.x.max(0) as usize;
+
+                    slf.highlight(start_point.x as usize, now_x);
+                },
+                MouseEvent::Drag {..} => {}
             }
         });
 
@@ -46,7 +59,7 @@ impl Widgetlike for InputBoxState {
         brush.bevel_w95(colors::Dark[0], colors::Light[3]);
         brush.putfs(&self.text);  // TODO: Don't wrap?
 
-        let cursor_region = brush.region(rect(self.cursor as isize, 0, 1, 2));
+        let cursor_region = brush.region(rect(self.cursor_l as isize, 0, (self.cursor_r as isize - self.cursor_l as isize + 1).max(1), 2));
 
         // make clickable
         brush.interactor(click_interactor, colors::Green[2], colors::Dark[0]).fill(FSem::new());
@@ -65,41 +78,85 @@ impl Widgetlike for InputBoxState {
 }
 impl InputBoxState {
     fn type_character(&mut self, character: char) {
-        if self.cursor < self.text.len() {
-            self.text.insert(self.cursor, character)
+        if self.cursor_l != self.cursor_r {
+            self.text.drain(self.cursor_l..self.cursor_r + 1);
+            self.cursor_r = self.cursor_l;
+        }
+
+        if self.cursor_l < self.text.len() {
+            self.text.insert(self.cursor_l, character)
         } else {
             self.text.push(character)
         }
-        self.cursor += 1;
+        self.cursor_l += 1;
+        self.cursor_r += 1;
     }
 
     fn backspace(&mut self) {
-        if self.cursor == 0 {
+        if self.cursor_l != self.cursor_r {
+            self.text.drain(self.cursor_l..self.cursor_r + 1);
+            self.cursor_r = self.cursor_l;
+            return
+        }
+
+        if self.cursor_l == 0 {
             return;
         }
         else {
-            self.text.remove(self.cursor - 1);
-            self.cursor -= 1;
+            self.text.remove(self.cursor_l - 1);
+            self.cursor_l -= 1;
+            self.cursor_r = self.cursor_l;
         }
     }
 
     fn delete(&mut self) {
-        if self.cursor >= self.text.len() {
+        if self.cursor_l != self.cursor_r {
+            self.text.drain(self.cursor_l..self.cursor_r + 1);
+            self.cursor_r = self.cursor_l;
+            return
+        }
+
+        if self.cursor_l >= self.text.len() {
             return;
         }
         else {
-            self.text.remove(self.cursor);
+            self.text.remove(self.cursor_l);
         }
     }
 
     fn set_cursor(&mut self, value: usize) {
-        self.move_cursor(value as isize - self.cursor as isize)
+        self.cursor_l = value;
+        self.cursor_r = value;
+        self.cursor_fixup()
     }
 
     fn move_cursor(&mut self, amount: isize) {
-        let mut cursor_2 = self.cursor as isize + amount;
+        if amount > 0 {
+            // move right, so start from right cursor
+            self.cursor_l = self.cursor_r;
+        }
+
+        let mut cursor_2 = self.cursor_l as isize + amount;
         if cursor_2 < 0 { cursor_2 = 0; }
         if cursor_2 > self.text.len() as isize { cursor_2 = self.text.len() as isize; }
-        self.cursor = cursor_2 as usize;
+        self.cursor_l = cursor_2 as usize;
+        self.cursor_r = self.cursor_l;
+    }
+
+    fn highlight(&mut self, i0: usize, i1: usize) {
+        let l = i0.min(i1);
+        let r = i0.max(i1);
+
+        self.cursor_l = l;
+        self.cursor_r = r;
+        self.cursor_fixup()
+    }
+
+    fn cursor_fixup(&mut self) {
+        if self.cursor_l > self.text.len() { self.cursor_l = self.text.len() }
+        if self.cursor_r > self.text.len() { self.cursor_r = self.text.len() }
+        if self.cursor_r != self.cursor_l && self.cursor_r == self.text.len() {
+            self.cursor_r = self.text.len() - 1
+        }
     }
 }
